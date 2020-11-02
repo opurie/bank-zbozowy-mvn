76e16475be2331299ec4e174251bd7804d1e4ce3
opurie <52296677+opurie@users.noreply.github.com>
2020-11-02 21:14:42 +0100

    poprawiony na 100

--- src/test/java/put/io/selenium/atm/AtmCardInfoTest.java
+++ src/test/java/put/io/selenium/atm/AtmCardInfoTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 public class AtmCardInfoTest {
 
 	private AtmCardInfo fixture = null;
-	
+
 	@Before
 	public void setUp() throws Exception {
 		fixture = new AtmCardInfo();
@@ -29,18 +29,18 @@ public class AtmCardInfoTest {
 	@Test
 	public void testTryPinCorrect() {
 		fixture.cardIn();
-		fixture.tryPin("1524");
+		fixture.tryPin("1525");
 		assertTrue(fixture.pinWasOk);
 	}
-	
+
 	@Test
 	public void testTryPinIncorrect() {
 		fixture.cardIn();
-		fixture.tryPin("1525");
+		fixture.tryPin("1524");
 		assertFalse(fixture.pinWasOk);
 		assertEquals(2, fixture.pinTriesLeft);
 	}
-	
+
 	@Test
 	public void testTryPinCardLocked() {
 		fixture.cardIn();
